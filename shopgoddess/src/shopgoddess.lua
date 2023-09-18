518b1e69cf63958396968caf0060c06b1f963f4a
lemoncloudtree <cloudsea8253@gmail.com>
2023-09-18 11:18:29 +0900

    증표 상점 열기

--- shopgoddess/src/shopgoddess.lua
+++ shopgoddess/src/shopgoddess.lua
@@ -1,6 +1,6 @@
 local author = 'lemoncloudtree'
 local addonName = 'shopgoddess'
--- version 1.0.1
+-- version 1.0.2
 
 _G['ADDONS'] = _G['ADDONS'] or {}
 _G['ADDONS'][author] = _G['ADDONS'][author] or {}
@@ -9,14 +9,24 @@ _G['ADDONS'][author][addonName] = _G['ADDONS'][author][addonName] or {}
 function SHOPGODDESS_ON_INIT(addon, frame)
     local acutil = require("acutil");
     acutil.slashCommand("/증표", SHOPGODDESS_COMMAND);
-
 end
 
 function SHOPGODDESS_COMMAND(command)
-  local cmd = "";
-
   if #command > 0 then
-    cmd = table.remove(command, 1);
+    local cmd_table = {
+        ["가비야"] = "REQ_GabijaCertificate_SHOP_OPEN()",
+        ["바카리네"] = "REQ_VakarineCertificate_SHOP_OPEN()",
+        ["라다"] = "REQ_RadaCertificate_SHOP_OPEN()",
+        ["바이보라"] = "REQ_DAILY_REWARD_SHOP_1_OPEN()",
+        ["용병단"] = "REQ_PVP_MINE_SHOP_OPEN()"
+    };
+
+    local cmd = cmd_table[tostring(table.remove(command, 1))];
+    if cmd ~= nil then
+      ui.CloseFrame('earthtowershop');
+      ReserveScript(cmd, 0.1);
+    end
+    
   else
     local msg = '';
     msg = msg.. '/증표  : 안내문{nl}';
@@ -25,30 +35,8 @@ function SHOPGODDESS_COMMAND(command)
     msg = msg.. '/증표 라다  : 라다 상점{nl}';
     msg = msg.. '/증표 바이보라  : 바이보라의 날개 상점{nl}';
     msg = msg.. '/증표 용병단  : 용병단 상점{nl}';
-    return ui.MsgBox(msg,"","Nope")
-  end
-
-  if cmd == "가비야" then
-    ui.CloseFrame('earthtowershop');
-    REQ_GabijaCertificate_SHOP_OPEN();
-    return;
-  elseif cmd == "바카리네" then
-    ui.CloseFrame('earthtowershop');
-    REQ_VakarineCertificate_SHOP_OPEN();
-    return;
-  elseif cmd == "라다" then
-    ui.CloseFrame('earthtowershop');
-    REQ_RadaCertificate_SHOP_OPEN();
-    return;
-  elseif cmd == "바이보라" then
-    ui.CloseFrame('earthtowershop');
-    REQ_DAILY_REWARD_SHOP_1_OPEN();
-    return;
-  elseif cmd == "용병단" then
-    ui.CloseFrame('earthtowershop');
-    REQ_PVP_MINE_SHOP_OPEN();
-    return;
+    return ui.MsgBox(msg,"","Nope");
   end
   
+  return;
 end
-
